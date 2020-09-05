a435389375c4d6912b34794c63fbb79795ed093b
Aishwarya Sawant <aishwaryasawant697@gmail.com>
2020-09-05 19:13:44 -0400

    Updates

---
+++ precache-manifest.327ed904d250b7f02259e8f1629c105b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c85df121654bb66c45c",
+    "url": "/Portfolio/static/css/main.947f5007.chunk.css"
+  },
+  {
+    "revision": "7c85df121654bb66c45c",
+    "url": "/Portfolio/static/js/main.7c85df12.chunk.js"
+  },
+  {
+    "revision": "4d20c02ae44627caa5f2",
+    "url": "/Portfolio/static/js/1.4d20c02a.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "133a3a6c001eae5e98e8e57234e30e1a",
+    "url": "/Portfolio/index.html"
+  }
+];
