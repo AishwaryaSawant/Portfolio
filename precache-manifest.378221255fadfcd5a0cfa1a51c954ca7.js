7669e5ff43821292e551870078c5fd64e384e9a3
Aishwarya Sawant <aishwaryasawant697@gmail.com>
2020-09-11 19:02:14 -0400

    Updates

--- precache-manifest.378221255fadfcd5a0cfa1a51c954ca7.js
+++ precache-manifest.378221255fadfcd5a0cfa1a51c954ca7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8df9d13f782297feb222",
+    "revision": "10ce4563c73eb0460a39",
     "url": "/Portfolio/static/css/main.6ca19a61.chunk.css"
   },
   {
-    "revision": "8df9d13f782297feb222",
-    "url": "/Portfolio/static/js/main.8df9d13f.chunk.js"
+    "revision": "10ce4563c73eb0460a39",
+    "url": "/Portfolio/static/js/main.10ce4563.chunk.js"
   },
   {
     "revision": "21569f65dffe88595d8b",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/runtime~main.3b961160.js"
   },
   {
-    "revision": "bd58667faf8d00f842e6117513698cf1",
+    "revision": "38bec358a19a3c164a5799df06bf7b58",
     "url": "/Portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.c5d879105d4b024dea85498fae511d33.js"
+  "/Portfolio/precache-manifest.378221255fadfcd5a0cfa1a51c954ca7.js"
 );
 
 workbox.clientsClaim();
