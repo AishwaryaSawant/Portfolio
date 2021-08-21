a622df4e4e1f7006b56a94b736842e2dbcee815d
AishwaryaSawant <aishwaryasawant697@gmail.com>
2021-08-21 10:39:06 -0400

    Updates

--- precache-manifest.72d4a7e26a672445e1e8444fa9c38c23.js
+++ precache-manifest.72d4a7e26a672445e1e8444fa9c38c23.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6935554264f6c762bac0",
+    "revision": "2fb1c90d05cab260045c",
     "url": "/Portfolio/static/css/main.6bd57229.chunk.css"
   },
   {
-    "revision": "6935554264f6c762bac0",
-    "url": "/Portfolio/static/js/main.69355542.chunk.js"
+    "revision": "2fb1c90d05cab260045c",
+    "url": "/Portfolio/static/js/main.2fb1c90d.chunk.js"
   },
   {
     "revision": "21569f65dffe88595d8b",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/runtime~main.3b961160.js"
   },
   {
-    "revision": "a00b55f627a261324c2a6ca49edf7ff8",
+    "revision": "5a65e1de8b73ca6218ca0d806c1916a5",
     "url": "/Portfolio/index.html"
   }
 ];
